9bd5b72229e7854281b1a862b630c37199c76ffe
Lauro Cabral <Lauro199471@gmail.com>
2018-04-16 05:52:42 -0700

    Added things

--- src/minimal_nodes/src/minimal_wiringPi.cpp
+++ src/minimal_nodes/src/minimal_wiringPi.cpp
@@ -8,6 +8,8 @@
 #include <stdio.h>
 #include <ros/ros.h>
 #include <iostream>
+#include "std_msgs/String.h"
+#include <sstream>
 
 using namespace std;
 
@@ -16,12 +18,14 @@ using namespace std;
 int main (int argc, char **argv)
 {
   int gpiopin = 18;
-
+  std_msgs::String msg;
+  std::stringstream ss;
   printf("Raspberry Pi wiringPi blink test\n");
 
   // *** ROS Stuff ****
   ros::init(argc,argv,"minimal_blink"); //name this node
   ros::NodeHandle n; // need this to establish communications with our new node
+  ros::Publisher chatter_pub = n.advertise<std_msgs::String>("LED_STATE", 1000);
   // ******************
 
   // Always initialise wiringPi. Use wiringPiSys() if you don't need
@@ -31,14 +35,24 @@ int main (int argc, char **argv)
 
   pinMode(gpiopin, OUTPUT);
 
+  ros::Rate loop_rate(10);
   while(ros::ok())
   {
     printf("LED On\n");
     digitalWrite(gpiopin, 1);
+    ss << "ON";
+    msg.data = ss.str();
+    chatter_pub.publish(msg);
+
     delay(one_sec);
     printf("LED Off\n");
     digitalWrite(gpiopin, 0);
+    ss << "OFF";
+    msg.data = ss.str();
+    chatter_pub.publish(msg);
+
     delay(one_sec);
+    loop_rate.sleep();
   }
 
   cout << "\nFinished\n";
