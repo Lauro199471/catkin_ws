f41f32ba1fa05295bf66b2af0a35e6a33686749d
Lauro Cabral <Lauro199471@gmail.com>
2018-04-20 04:35:15 -0700

    Added Stuff

--- src/minimal_nodes/src/minimal_pwm.cpp
+++ src/minimal_nodes/src/minimal_pwm.cpp
@@ -7,9 +7,6 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
-  int i = 0;
-  int values[] = {0,25,50,75,100,75,50,25};
-
   ros::init(argc,argv,"minimal_pwm"); //name this node
   // when this compiled code is run, ROS will recognize it as a node called "minimal_wiringPi"
 
@@ -18,21 +15,12 @@ int main(int argc, char **argv)
   cout << "Raspberry Pi wiringPi test program\n";
   wiringPiSetupGpio(); // Initalize Pi
 
-  //softPwmCreate (int pin, int initialValue, int pwmRange)
-  softPwmCreate(18,0,100);
-
-
-  while(ros::ok()) // Ctrl-C Handler
-  {
-    for(i = 0 ; i < 8 ; i++ )
-    {
-      softPwmWrite(18,values[i]);
-    }
-
-    delay(1000);
-  }
-
-  softPwmWrite(18,values[0]);
-  cout << "Finished" << endl;
-  return 0; // should never get here, unless roscore dies
+  pinMode (18, PWM_OUTPUT) ;
+  pwmSetMode (PWM_MODE_MS);
+  pwmSetRange (2000);
+  pwmSetClock (192);
+  pwmWrite(18,150);
+  delay(1000);
+  pwmWrite(18,200);
+  return 0;
 }
